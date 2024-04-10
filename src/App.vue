73f369453199b4530c1c0e98e42f523e0bd5e0e9
danielbosa <danielbosa95@gmail.com>
2024-04-10 21:34:40 +0200

    Footer three component: static

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <MainComponent/>
     <FooterOneComponent/>
     <FooterTwoComponent/>
+    <FooterThreeComponent/>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
 import FooterOneComponent from './components/FooterOneComponent.vue';
 import FooterTwoComponent from './components/FooterTwoComponent.vue';
+import FooterThreeComponent from './components/FooterThreeComponent.vue';
 
   export default {
     name: 'App',
@@ -20,6 +22,7 @@ import FooterTwoComponent from './components/FooterTwoComponent.vue';
       MainComponent,
       FooterOneComponent,
       FooterTwoComponent,
+      FooterThreeComponent,
     },
     // data(){
     //   return{
---
+++ src/components/FooterThreeComponent.vue
@@ -0,0 +1,81 @@
+<template>
+    <main>
+        <div class="db-container">
+            <div class="cta">
+                sign-up now!
+            </div>
+            <div class="footer-icons">
+                <div>follow us</div>
+                <div>
+                    <img src="/public/img/footer-facebook.png" alt="">
+                </div>
+                <div>
+                    <img src="/public/img/footer-twitter.png" alt="">
+                </div>
+                <div>
+                    <img src="/public/img/footer-youtube.png" alt="">
+                </div>
+                <div>
+                    <img src="/public/img/footer-pinterest.png" alt="">
+                </div>
+                <div>
+                    <img src="/public/img/footer-periscope.png" alt="">
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'FooterTwoComponent',
+        // data() {
+        //     return{
+
+        //     }
+        // },
+    }
+</script>
+
+<style lang="scss" scoped>
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        background-color: #303030;
+        font-size: small;
+    }
+
+    .db-container{
+        max-width: 80%;
+        margin: 0 auto;
+        padding: 30px 0;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .cta{
+        border: 3px solid #0c7cec;
+        padding: 10px;
+        color: white;
+        text-transform: uppercase;
+        font-weight: bold;
+    }
+
+    .footer-icons{
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        text-transform: uppercase;
+        font-weight: bold;
+        color: #0c7cec;
+        i{
+            background-color: #797979;
+            color: transparent;
+        };
+        img{
+            width: 30px;
+        }
+    }
+
+</style>
