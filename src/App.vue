46418eacefeae53f867f3195c5ec305f38ce2052
danielbosa <danielbosa95@gmail.com>
2024-04-10 16:31:40 +0200

    Scaffolding

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ExampleComponent/>
+  </div>
+</template>
+
+<script>
+import ExampleComponent from './components/ExampleComponent.vue';
+  export default {
+    name: 'App',
+    components: {
+      ExampleComponent,
+    },
+    data(){
+      return{
+        
+      }
+    },
+    methods: {
+
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
