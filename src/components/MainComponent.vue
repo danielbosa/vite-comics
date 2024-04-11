ed302ac6a1ec6d7e72cf2be320830108f8813277
danielbosa <danielbosa95@gmail.com>
2024-04-11 18:59:13 +0200

    Styling fixed

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <div class="db-container pb-4">
-            <div class="container py-5">
+            <span class="db-btn db-btn-1">Current series</span>
+            <div class="container py-4">
                 <div class="row g-3">
                     <div class="col-4 col-md-3 col-lg-2 h-100" v-for="(item,index) in items" :key="index">
                         <CardComponent :image="item.thumb" :title="item.series"/>
@@ -9,7 +10,7 @@
                 </div>
             </div>
             <div class="text-center">
-                <button class="db-btn">Load more</button>
+                <button class="db-btn db-btn-2">Load more</button>
             </div>    
         </div>
     </div>
@@ -45,14 +46,23 @@ import CardComponent from './CardComponent.vue';
         div{
             color: $light;
         }
+        span.db-btn{
+            display: inline-block;
+            transform: translate(0,-50%);
+        }
     }
 
     .db-btn{
-        padding: 5px 50px;
-        background-color: $accenture;
-        color: $light;
         text-transform: uppercase;
         font-weight: bold;
+        background-color: $accenture;
+        color: $light;
+        &.db-btn-1{
+            padding: 10px 20px;
+        }
+        &.db-btn-2{
+            padding: 5px 50px;
+        }
     }
     
 </style>
